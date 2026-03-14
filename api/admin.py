58b2d30f74e5fa44f4666ac2001484af50867e86
nguyenbrian430-art <nguyenbrian430@gmail.com>
2026-03-14 18:50:21 -0400

    models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Class, Assignment
 
-# Register your models here.
+
+admin.site.register(Class)
+admin.site.register(Assignment)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Class(models.Model):
+    className=models.CharField(max_length=50)
+
+class Assignment(models.Model):
+    className=models.ForeignKey(Class, on_delete=models.CASCADE)
+    # user=models.ForeignKey(User, on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    grade=models.FloatField(validators=[MinValueValidator(0)])
